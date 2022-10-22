62712864d173825f1ef1b0e571a5c5aa9d4665be
mateuslopesreis <mateus.reis@estudante.ifms.edu.br>
2022-10-22 20:53:25 -0300

    ✨ Funcionalidade: Adicionar Tarefa

--- src/js/lista-tarefas.js
+++ src/js/lista-tarefas.js
@@ -1,8 +1,18 @@
 import './nav.js'
 import '../css/lista-tarefas.css'
 
-document.querySelector('seletor')
+const bnt = document.querySelector('#btn-adicionar')
 
-elemento.addEventListener('evento',funcao({
-    
-}))
+bnt.addEventListener('click', function(){
+    let li = document.createElement('li')
+    li.setAttribute('class','tarefa')
+
+    let lista = document.querySelector('#lista-tarefas')
+    let texto = document.querySelector('#input-nova-tarefa')
+
+    li.innerHTML = `
+	<div>${texto.value}</div>
+  <button class="btn-excluir">Excluir</button>
+`
+  lista.appendChild(li)
+});
