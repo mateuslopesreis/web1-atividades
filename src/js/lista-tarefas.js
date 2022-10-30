f64e75314e8d03bf1cd79a9e96ff153a9453dab8
mateuslopesreis <mateus.reis@estudante.ifms.edu.br>
2022-10-30 12:03:15 -0300

    ✨ Funcionalidade: Remover Tarefa

--- src/js/lista-tarefas.js
+++ src/js/lista-tarefas.js
@@ -2,17 +2,41 @@ import './nav.js'
 import '../css/lista-tarefas.css'
 
 const bnt = document.querySelector('#btn-adicionar')
+let lista = document.querySelector('#lista-tarefas')
+let texto = document.querySelector('#input-nova-tarefa')
 
 bnt.addEventListener('click', function(){
     let li = document.createElement('li')
     li.setAttribute('class','tarefa')
 
-    let lista = document.querySelector('#lista-tarefas')
-    let texto = document.querySelector('#input-nova-tarefa')
+   
 
     li.innerHTML = `
 	<div>${texto.value}</div>
   <button class="btn-excluir">Excluir</button>
 `
   lista.appendChild(li)
+
+  texto.value="";
+  texto.focus();
+
+  
+
 });
+
+
+let li = document.createElement('li')
+// adicionar um atributo ao elemento
+li.setAttribute('class','tarefa')
+
+
+console.log(lista)
+lista.addEventListener('click',function(event){
+
+  if(event.target.className === 'btn-excluir'){
+    let tarefa = (event.target).parentNode
+    
+    lista.removeChild(tarefa)
+  }
+
+})
