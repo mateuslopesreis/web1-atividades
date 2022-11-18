0edbb0efa2c5769559aa81481232cf83e5047124
mateuslopesreis <mateus.reis@estudante.ifms.edu.br>
2022-11-18 20:34:57 -0300

    Preenchimento dos dados no cartão virtual

--- src/js/pagamento.js
+++ src/js/pagamento.js
@@ -1,2 +1,23 @@
 import './nav.js'
 import '../css/pagamento.css'
+
+const inputNumeroCartao = document.querySelector('#mudaTam')
+const inputNomeCartao = document.querySelector('#mudaTam1')
+const inputDataCartao = document.querySelector('#mudaTam2')
+const divNumeroCartaoImg = document.querySelector('.card-number')
+const divNomeCartao = document.querySelector('.card-holder')
+const divDataCartao = document.querySelector('.expiration')
+
+inputNumeroCartao.addEventListener('input', function(){
+    divNumeroCartaoImg.innerHTML = inputNumeroCartao.value;
+    divNomeCartao.innerHTML = inputCartao.value;
+    divDataCartao.innerHTML = inputCartao.value;
+})
+
+inputNomeCartao.addEventListener('input', function(){
+    divNomeCartao.innerHTML = inputNomeCartao.value;
+})
+
+inputDataCartao.addEventListener('input', function(){
+    divDataCartao.innerHTML = inputDataCartao.value;
+})
